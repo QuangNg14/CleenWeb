dbb64b2ec2e969423ca5a183b8be755a50e36405
MRSNOO <nnquang.1412@gmail.com>
2019-10-23 21:44:42 +0700

    .

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,4 +1,4 @@
-const addClassFunc = (element, nameClass) => {
+function addClassFunc (element, nameClass) {
   element.classList.add(nameClass);
 }
 
@@ -10,6 +10,29 @@ if ( window.location.pathname === '/home' || window.location.pathname === '/home
   currentPage = parseInt(window.location.pathname.replace('/home/', ''));
   // console.log(currentPage);
   addClassFunc(pointArrowDOM[currentPage-1], 'a1')
+}
+
+let headerRight = document.getElementById("header_right")
+let btnSwitch1 = headerRight.getElementsByTagName("a")[0]
+let btnSwitch2 = headerRight.getElementsByTagName("a")[1]
+if(window.location.pathname === '/home'){
+  addClassFunc(btnSwitch2,"left")
+}
+// console.log(window.location.pathname)
+else if(window.location.pathname === '/'){
+  addClassFunc(btnSwitch1,"left")
+}
+
+function onClickHandler(){
+  addClassFunc(btnSwitch1,"left")
+  btnSwitch2.className = `login text_style`
+}
+
+function onClickHandler2(){
+  addClassFunc(btnSwitch2,"left")
+  // console.log(window.location.pathname)
+  btnSwitch1.className = `login text_style`
+
 }
 
 // hightlightBottomDom = document.getElementsByClassName('active');
